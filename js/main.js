bea4879f36764edbe50252d504e7997ee485fe83
maksymova <en.maksymova@gmail.com>
2018-03-23 20:13:06 +0200

    release version

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+            var carousel = $(".carousel").waterwheelCarousel({
+                flankingItems: 3
+            });
+            $("#prev").bind('click', function() {
+                carousel.prev();
+                return false;
+            });
+            $("#next").bind('click', function() {
+                carousel.next();
+                return false;
+            });
+        });
